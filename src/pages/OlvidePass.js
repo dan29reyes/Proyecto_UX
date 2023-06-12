833d7e1d391de9193f47cb1749b19a767aee5e0e
Franklin Rodriguez <xaviertinoco006@unitec.edu>
2023-06-12 10:40:43 -0600

    js_olvidepass

---
+++ src/pages/OlvidePass.js
@@ -0,0 +1,62 @@
+
+
+import React, { useState } from 'react';
+import '../styles/olvidepassword.css';
+import axios from 'axios';
+import services from '../utils/services';
+import validator from '../utils/validator';
+import { useNavigate } from 'react-router-dom';
+
+const OlvidePasswordPage = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: '',
+  });
+
+  const handlePasswordReset = async (e) => {
+    e.preventDefault();
+    if (!validator.isEmail(form.email)) {
+      console.log("email bad");
+    } else {
+      try {
+        const response = await services.postPasswordReset(form.email);
+        console.log("Check your email for a password reset link");
+        navigate("/Login");
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          console.log("Email not found");
+        } else {
+          console.log("An error occurred:", error);
+        }
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  return (
+    <div className="reset-container">
+      <form onSubmit={handlePasswordReset}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="input-field"
+          onChange={handleChange}
+        />
+        <button type="submit" className="reset-button">
+          Reset Password
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default OlvidePasswordPage;
