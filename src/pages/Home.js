abd3dd3630a682f1dc1ceac35825ebfc70c4a22f
Franklin Rodriguez <xaviertinoco006@unitec.edu>
2023-06-18 16:24:13 -0600

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,40 @@
-
 import React, { useState } from 'react';
-import '../styles/Home.css';
+import "../styles/Home.css";
+
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const HomePage = () => {
   const navigate = useNavigate();
 
-    const handleLogin = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
     navigate("/InicioSesion");
-    }
-    const handleRegister = (e) => {
+  };
+
+  const handleRegister = (e) => {
     e.preventDefault();
-    navigate("/Registro");
-    }
-    
-    
-  
+    navigate("/Registro"); 
+  };
 
   return (
     <div>
-      <h1>Home Page</h1>
-      
+    <div className="home">
+      <ul>
+      <li><a href="Trollos">Trollo</a></li>
+        <li><a href="/InicioSesion" onClick={handleLogin}>login</a></li>
+        <li><a href="/Registro" onClick={handleRegister}>Register</a></li>
+        <li><a href="about Us">About Us</a></li>
+      </ul>
+      </div>
+      <section className="container">
+  <div className="wave"> </div>
+    </section>
       <footer>
-            <p>&copy; Kenneth Reyes / Franklin Rodriguez</p>
-          </footer>
+        <p>&copy; Kenneth Reyes / Franklin Rodriguez</p>
+      </footer>
     </div>
+   
     
   );
 };
