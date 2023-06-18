21166e88a2fee986de0620d381d057ea5a0225ad
Franklin Rodriguez <xaviertinoco006@unitec.edu>
2023-06-18 16:49:23 -0600

    Mas Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,60 @@
 import React, { useState } from 'react';
-import "../styles/Home.css";
-
 import { useNavigate } from 'react-router-dom';
+import trollo from "../images/Trollo.jpeg";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Home.css';
 
 const HomePage = () => {
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
+  const handleNavigation = (e, route) => {
     e.preventDefault();
-    navigate("/InicioSesion");
-  };
-
-  const handleRegister = (e) => {
-    e.preventDefault();
-    navigate("/Registro"); 
+    navigate(route);
   };
 
   return (
-    <div>
-    <div className="home">
-      <ul>
-      <li><a href="Trollos">Trollo</a></li>
-        <li><a href="/InicioSesion" onClick={handleLogin}>login</a></li>
-        <li><a href="/Registro" onClick={handleRegister}>Register</a></li>
-        <li><a href="about Us">About Us</a></li>
-      </ul>
+    <div className="container-fluid">
+      <div className="background">
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
       </div>
+
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#" onClick={(e) => handleNavigation(e, "/")}>Home</a></li>
+            <li><a href="#" onClick={(e) => handleNavigation(e, "/InicioSesion")}>Login</a></li>
+            <li><a href="#" onClick={(e) => handleNavigation(e, "/Registro")}>Register</a></li>
+            <li><a href="#" onClick={(e) => handleNavigation(e, "/About")}>About Us</a></li>
+            
+          </ul>
+        </nav>
+        <div className="logo">
+            
+            <img src={trollo} width={100} alt="Login" style={{marginRight:'105px'}} />
+            
+          </div>
+        
+          
+        
+        
+
+        <section className="header-content">
+          <h1>Bienvenido a Trollo</h1>
+          <p>Welcome to our studio. We are a passionate group of people, making high-quality products designed to make your life easier.</p>
+          
+        </section>
+      </header>
+
       <section className="container">
-  <div className="wave"> </div>
-    </section>
-      <footer>
-        <p>&copy; Kenneth Reyes / Franklin Rodriguez</p>
-      </footer>
+        <div className="wave"></div>
+      </section>
+
+      
     </div>
-   
-    
   );
 };
 
